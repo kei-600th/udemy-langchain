c08fc563aac9642fc831da0a57201a4cb1e955a2
kei-600th <kei19941005@gmail.com>
2023-12-19 15:52:37 +0900

    slackbotがLLMを使うようにする

--- src/slack_app.py
+++ src/slack_app.py
@@ -1,10 +1,15 @@
 import os
 
+from chatbot_engine import chat, create_index
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+from langchain.memory import ChatMessageHistory
+
 load_dotenv()
+
+index = create_index()
 
 # ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
@@ -12,7 +17,11 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 @app.event("app_mention")
 def handle_mention(event, say):
-    say("Hello Slack!")
+    history = ChatMessageHistory()
+
+    message = event["text"]
+    bot_message = chat(message, history, index)
+    say(bot_message)
 
 
 # アプリを起動します
